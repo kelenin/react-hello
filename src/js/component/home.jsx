2e05ffa602991d91ed709fee4dde063f079178d1
Katheren Salom <katherensalom@gmail.com>
2022-08-06 23:40:15 +0000

    modified:   package-lock.json modified:   package.json modified:   src/js/component/home.jsx new file:   src/styles/app.css

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import App from "./app.jsx";
+import "../../styles/app.css";
 
 //create your first component
 const Home = () => {
